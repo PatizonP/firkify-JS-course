fcf437b5f61cff7baa1f0b6baaedab92395ec49e
patizonP <patriprokop@gmail.com>
2021-01-30 15:35:59 +0100

    Test commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -117,6 +117,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Hello');
+  console.log('Hell');
 };
 init();
